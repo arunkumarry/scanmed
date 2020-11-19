1b0d3bfc6942d497930056f0390595a79e027c14
arun kumar <arunkumar.ry1@gmail.com>
2020-11-20 00:15:37 +0530

    converted to es6 and added nodemon

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
+import express from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import indexRouter from "./routes/index";
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/v1', indexRouter);
+app.use("/v1", indexRouter);
 
-module.exports = app;
+export default app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+import express from "express";
+
+const indexRouter = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  return res.status(200).send({message: 'Welcome to scanmed!!!'});
+indexRouter.get("/", (req, res, next) => {
+  return res.status(200).send({ message: "Welcome to scanmed!!!" });
 });
 
-module.exports = router;
+export default indexRouter;
